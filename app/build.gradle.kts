12a1331a0f069dbe2fb8ddb71b32c81dec944e74
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-02-03 15:02:35 +0530

    new sub module created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,22 +48,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-
-    //material design dependency
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    //navigation dependencies
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 
 
     //dagger dependenciesg
@@ -72,6 +59,6 @@ dependencies {
     kapt ("com.google.dagger:dagger-android-processor:2.44.2")
 
     //base module access
-    implementation(project(":base"))
+    implementation(project(":base-sub"))
 
 }
--- base/src/main/java/com/example/app/BaseApp.kt
+++
@@ -1,4 +0,0 @@
-package com.example.app
-
-abstract class BaseApp {
-}
