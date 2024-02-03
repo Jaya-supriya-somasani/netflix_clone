474c63c5824950772b91e3dc969505b61e1d4032
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-02-03 20:23:20 +0530

    Base App file and netflix app files are created

---
+++ app/src/main/java/com/example/netflix_clone/ui/di/AppComponent.kt
@@ -0,0 +1,8 @@
+package com.example.netflix_clone.ui.di
+
+import com.example.netflix_clone.ui.app.NetflixApp
+import dagger.android.AndroidInjector
+
+
+interface AppComponent : AndroidInjector<NetflixApp> {
+}
