646e7418fe82d7bccd68fcf2bf828f4829087307
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-02-03 20:23:20 +0530

    Base App file and netflix app files are created

---
+++ app/src/main/java/com/example/netflix_clone/ui/app/NetflixApp.kt
@@ -0,0 +1,11 @@
+package com.example.netflix_clone.ui.app
+
+import com.example.base.BaseApp
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+class NetflixApp : BaseApp() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        TODO("Not yet implemented")
+    }
+}
