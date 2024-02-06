d5c1554bf2379be7e8048324909cb464229a1c7b
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-02-06 15:49:39 +0530

    dagger 2 set up

--- app/src/main/java/com/example/netflix_clone/ui/app/NetflixApp.kt
+++ app/src/main/java/com/example/netflix_clone/ui/app/NetflixApp.kt
@@ -1,11 +1,13 @@
 package com.example.netflix_clone.ui.app
 
-import com.example.base.BaseApp
+import com.example.base.app.BaseApp
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
 class NetflixApp : BaseApp() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        TODO("Not yet implemented")
+    return DaggerAppComponent.builder()
+        .application(this)
+        .build()
     }
 }
--- app/src/main/java/com/example/netflix_clone/ui/di/AppComponent.kt
+++ app/src/main/java/com/example/netflix_clone/ui/di/AppComponent.kt
@@ -1,8 +1,33 @@
 package com.example.netflix_clone.ui.di
 
+import android.app.Application
 import com.example.netflix_clone.ui.app.NetflixApp
+import com.example.netflix_clone.ui.module.AppModule
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
 import dagger.android.AndroidInjector
+import javax.inject.Singleton
 
-
+/**
+ * AndroidInjectionModule is a Dagger module necessary for Dagger Android support.*/
+@Singleton
+@Component(
+    modules = [AndroidInjectionModule::class, AppModule::class]
+)
 interface AppComponent : AndroidInjector<NetflixApp> {
+    /**
+     * @Component.Builder is a part of the component and will be used to construct instances of AppComponent.*/
+    @Component.Builder
+    interface Builder {
+
+        /**
+         * allow binding an instance  (in this case, Application) to the component during construction.
+        This is useful when you need to pass an instance at component creation time.
+        It's called after setting all the necessary bindings.*/
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
 }
