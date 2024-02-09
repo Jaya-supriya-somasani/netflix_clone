1991786d7caa9a7cdbe98d3c5cf628620a012d14
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-02-09 15:11:51 +0530

    BaseApp is implemented

--- app/src/main/java/com/example/netflix_clone/app/NetflixApp.kt
+++ app/src/main/java/com/example/netflix_clone/app/NetflixApp.kt
@@ -1,14 +1,13 @@
 package com.example.netflix_clone.app
 
-import android.app.Application
 import com.example.base.app.BaseApp
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
-class NetflixApp : Application() {
-//    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-//    return DaggerAppComponent.builder()
-//        .application(this)
-//        .build()
-//    }
+class NetflixApp : BaseApp() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+    return DaggerAppComponent.builder()
+        .application(this)
+        .build()
+    }
 }
