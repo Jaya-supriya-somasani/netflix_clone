f750ab5bd225ec23987f766e431ac78489afc31d
Jai Keerthick <jaikeerthick@gmail.com>
2024-02-10 19:42:17 +0530

    [Fix] jdk upgrade

--- app/src/main/java/com/example/netflix_clone/app/NetflixApp.kt
+++ app/src/main/java/com/example/netflix_clone/app/NetflixApp.kt
@@ -1,6 +1,7 @@
 package com.example.netflix_clone.app
 
 import com.example.base.app.BaseApp
+import com.example.netflix_clone.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
