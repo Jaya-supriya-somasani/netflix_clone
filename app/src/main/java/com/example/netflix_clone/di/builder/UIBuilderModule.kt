1d4f597e2761d7f3a52c1a93147cd1b1d277bd2d
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-02-11 11:42:39 +0530

    dagger 2 issue fixed

--- app/src/main/java/com/example/netflix_clone/di/builder/UIBuilderModule.kt
+++ app/src/main/java/com/example/netflix_clone/di/builder/UIBuilderModule.kt
@@ -1,15 +1,9 @@
 package com.example.netflix_clone.di.builder
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.example.base.viewmodel.ViewModelKey
 import com.example.netflix_clone.ui.main.MainActivity
 import com.example.netflix_clone.ui.main.MainActivityModule
-import com.example.netflix_clone.ui.main.MainActivityViewModel
 import dagger.Module
-import dagger.Provides
 import dagger.android.ContributesAndroidInjector
-import dagger.multibindings.IntoMap
 
 @Module
 abstract class UIBuilderModule {
