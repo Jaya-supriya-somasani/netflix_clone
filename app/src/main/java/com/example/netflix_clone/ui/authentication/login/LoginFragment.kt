8acba55412324efcfdf92eb026063b5d69d2a97a
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-02-12 19:13:42 +0530

    Authentication and Splash Activities created

--- app/src/main/java/com/example/netflix_clone/ui/authentication/login/LoginFragment.kt
+++ app/src/main/java/com/example/netflix_clone/ui/authentication/login/LoginFragment.kt
@@ -1,17 +1,9 @@
 package com.example.netflix_clone.ui.authentication.login
 
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.LifecycleOwner
-import androidx.navigation.Navigation
 import com.example.base.BaseFragment
 import com.example.netflix_clone.R
 import com.example.netflix_clone.databinding.FragmentLoginBinding
-import dagger.android.AndroidInjector
 
 class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
     override fun initObservers(viewLifecycleOwner: LifecycleOwner) {
