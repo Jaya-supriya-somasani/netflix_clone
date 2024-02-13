3966efb73d8f86f9d310790ac13d532a781f1685
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-02-13 23:48:21 +0530

    login page navigation is added to onBoard screen

--- app/src/main/java/com/example/netflix_clone/ui/authentication/onBoard/OnBoardViewModel.kt
+++ app/src/main/java/com/example/netflix_clone/ui/authentication/onBoard/OnBoardViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.netflix_clone.ui.authentication.onBoard
 
 import com.example.base.BaseViewModel
+import com.example.base.utils.ConflatedChannel
+import com.example.base.utils.sendValue
 
 class OnBoardViewModel : BaseViewModel() {
+    val loginEventClick = ConflatedChannel<Unit>()
+
+    fun navigateToLoginScreen() {
+        loginEventClick.sendValue(Unit)
+    }
 }
