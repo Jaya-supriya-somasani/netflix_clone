a9cceb91922e996d03a1ca0b2b1853f0cc07c03c
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-02-13 23:48:21 +0530

    login page navigation is added to onBoard screen

--- app/src/main/java/com/example/netflix_clone/ui/authentication/onBoard/OnBoardFragment.kt
+++ app/src/main/java/com/example/netflix_clone/ui/authentication/onBoard/OnBoardFragment.kt
@@ -1,7 +1,12 @@
 package com.example.netflix_clone.ui.authentication.onBoard
 
+import android.util.Log
 import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.fragment.findNavController
 import com.example.base.BaseFragment
+import com.example.base.utils.collect
+import com.example.base.utils.safeLaunch
+import com.example.base.utils.safeLaunchWhenResumed
 import com.example.netflix_clone.R
 import com.example.netflix_clone.databinding.FragmentOnBoardBinding
 
@@ -11,8 +16,21 @@ class OnBoardFragment : BaseFragment<FragmentOnBoardBinding, OnBoardViewModel>()
     override fun getLayoutResource() = R.layout.fragment_on_board
 
     override fun initObservers(viewLifecycleOwner: LifecycleOwner) {
+        safeLaunchWhenResumed {
+            viewModel.loginEventClick.collect {
+                navigateToLoginScreen()
+            }
+        }
+    }
+
+    private fun navigateToLoginScreen() {
+        Log.d(TAG,"Login-event-clicked")
+        val action = OnBoardFragmentDirections.actionOnBoardFragmentToLoginFragment()
+        findNavController().navigate(action)
     }
 
     override fun setUp() {
+        dataBinding.viewModel=viewModel
+
     }
 }
