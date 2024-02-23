d375c5ad694fb4f76de4d8c2c9116b4ad501b287
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-02-23 12:12:09 +0530

    remove sub-base module from project level

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,6 @@ dependencies {
     kapt("com.google.dagger:dagger-android-processor:2.44.2")
 //    kapt("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.5.0")
     //base module access
-    implementation(project(":base-sub"))
+//    implementation(project(":base-sub"))
 
 }
