8eb57e402da1998981ab3c343a0f408f28335a5f
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2024-02-26 12:05:19 +0530

    validations added for sign in user

--- app/src/main/java/com/example/netflix_clone/ui/authentication/login/LoginFragment.kt
+++ app/src/main/java/com/example/netflix_clone/ui/authentication/login/LoginFragment.kt
@@ -3,17 +3,44 @@ package com.example.netflix_clone.ui.authentication.login
 import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.fragment.findNavController
 import com.example.base.BaseFragment
+import com.example.base.utils.collect
+import com.example.base.utils.safeLaunchWhenResumed
 import com.example.netflix_clone.R
 import com.example.netflix_clone.databinding.FragmentLoginBinding
 
 class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
     override fun initObservers(viewLifecycleOwner: LifecycleOwner) {
+        safeLaunchWhenResumed {
+            viewModel.signUpEvent.collect {
+                val action = LoginFragmentDirections.actionLoginFragmentToSignUpFragment()
+                findNavController().navigate(action)
+            }
+        }
+        safeLaunchWhenResumed {
+            viewModel.credentialsValidateEvent.collect {
+                validateCredentials()
+            }
+        }
     }
 
     override fun setUp() {
         dataBinding.viewModel = viewModel
         dataBinding.arrowBackIcon.setOnClickListener {
             findNavController().popBackStack()
+        }
+    }
+
+    private fun validateCredentials() {
+        if (!viewModel.emailOrPhoneNumberFlow.value.contains("@gmail.com")) {
+            dataBinding.emailNumberTextLayout.error = "Enter correct email or phone number"
+            dataBinding.passwordTextLayout.error = ""
+        } else if (viewModel.passwordFlow.value.isBlank() || viewModel.passwordFlow.value.length >= 4) {
+            dataBinding.passwordTextLayout.error =
+                "Your password must contain between 4 to 60 characters"
+            dataBinding.emailNumberTextLayout.error = ""
+
+        } else {
+            viewModel.signInUser()
         }
     }
 
--- app/src/main/java/com/example/netflix_clone/ui/authentication/login/LoginViewModel.kt
+++ app/src/main/java/com/example/netflix_clone/ui/authentication/login/LoginViewModel.kt
@@ -1,8 +1,28 @@
 package com.example.netflix_clone.ui.authentication.login
 
-import androidx.lifecycle.ViewModel
+import android.content.ContentValues.TAG
+import android.util.Log
 import com.example.base.BaseViewModel
+import com.example.base.utils.ConflatedChannel
+import com.example.base.utils.sendValue
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class LoginViewModel : BaseViewModel() {
-    // TODO: Implement the ViewModel
+    val signUpEvent = ConflatedChannel<Unit>()
+    val emailOrPhoneNumberFlow = MutableStateFlow("")
+    val passwordFlow = MutableStateFlow("")
+    val credentialsValidateEvent = ConflatedChannel<Unit>()
+
+    fun navigateToSignup() {
+        signUpEvent.sendValue(Unit)
+    }
+
+    fun navigateToSignIn() {
+        credentialsValidateEvent.sendValue(Unit)
+    }
+
+    fun signInUser() {
+        Log.d(TAG, "Sign-in email ${emailOrPhoneNumberFlow.value} password ${passwordFlow.value}")
+    }
+
 }
